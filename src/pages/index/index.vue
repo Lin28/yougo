652ce7733f78513a718cfe53697ffc47f83e9268
Gayqq <835384040@qq.com>
2018-12-09 17:17:24 +0800

    完成头部搜索框

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,26 @@
 <template>
-   <div>首页</div>
+   <div class="index">
+      <div class="nav">
+         <input type="text" placeholder="搜索">
+      </div>
+   </div>
 </template>
 <script>
 export default{
 
 }
 </script>
-<style>
-  
+<style lang="scss">
+$uRed:#ff2d4a;
+.nav{
+   padding: 20rpx 16rpx;
+   background-color: $uRed;
+   input{
+      background-color: white;
+      height: 60rpx;
+      text-align: center;
+      font-size: 24rpx;
+      color: #bbb;
+   }
+}
 </style>
